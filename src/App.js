9631a117ee4913fa95bec05ef8ffc13b91893083
beckibloom <rblumer13@gmail.com>
2020-08-15 08:23:53 -0500

    added standard CSS and outlined App.js structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import React from 'react';
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <body>
+        {/* Login screen component */}
+        {/* Sign up screen component */}
+        {/* Welcome 1 component */}
+        {/* Welcome 2 component */}
+        {/* Welcome 3 component */}
+        {/* Welcome 4 component */}
+        {/* Profile component */}
+        {/* Home component */}
+        {/* Menu component */}
+        {/* Recipe component */}
+        {/* Matches component */}
+        {/* Filter component */}
+        {/* Favorites component */}
+        </body>
     </main>
   );
 }
